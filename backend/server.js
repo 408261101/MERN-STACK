74099730d523ef418752b447c7d4ca245a6ba81a
408261101 <chenxiwei96@gmail.com>
2024-01-17 12:03:36 +0800

    Run App On 4000

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const menuRoutes = require('./routes/menu')
+const path = require('path');
 const PORT = 4000;
 
 //express app
@@ -13,6 +14,14 @@ app.use(express.json())
 
 // routes
 app.use('/api/menu', menuRoutes)
+
+// 提供靜態檔案
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// 將所有非 API 路徑指向 React 應用
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 //connect to mongodb
 mongoose.connect(process.env.DB_URI)
